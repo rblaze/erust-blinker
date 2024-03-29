1523616806d74aa1c5e9272d5519076f5949fcaa
Andrey Sverdlichenko <blaze@rusty.zone>
2024-03-28 23:33:31 -0400

    Update to G071

---
+++ memory.x
@@ -0,0 +1,6 @@
+/* Linker script for the STM32L432KC */
+MEMORY
+{
+  FLASH : ORIGIN = 0x08000000, LENGTH = 128K
+  RAM : ORIGIN = 0x20000000, LENGTH = 32K
+}
--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,36 @@
 #![no_std]
 #![no_main]
 
-// use panic_probe as _;
-use panic_halt as _;
+use panic_probe as _;
+// use panic_halt as _;
 
 use cortex_m_rt::entry;
-use stm32l0xx_hal::pac;
-use stm32l0xx_hal::prelude::*;
-use stm32l0xx_hal::rcc::Config;
+use rtt_target::rprintln;
+use rtt_target::rtt_init_print;
+use stm32g0xx_hal::pac;
+use stm32g0xx_hal::prelude::*;
+use stm32g0xx_hal::rcc::{Config, Prescaler};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let dp = pac::Peripherals::take().unwrap();
 
     // Configure the clock.
-    let mut rcc = dp.RCC.freeze(Config::hsi16());
+    let mut rcc = dp.RCC.freeze(Config::hsi(Prescaler::Div16));
 
     // Acquire the GPIO peripherals. This also enables the clock for GPIO in
     // the RCC register.
     let gpioa = dp.GPIOA.split(&mut rcc);
-    let gpioc = dp.GPIOC.split(&mut rcc);
 
     // Configure PA5 as output.
     let mut led = gpioa.pa5.into_push_pull_output();
 
-    // Configure PC13 as input.
-    let button = gpioc.pc13.into_floating_input();
-
     loop {
-        let state = match button.is_low() {
-            Ok(true) => PinState::High,
-            Ok(false) => PinState::Low,
-            _ => unreachable!(),
-        };
-
-        led.set_state(state).unwrap();
+        rprintln!("loop");
+        cortex_m::asm::delay(200_000);
+        led.set_high().unwrap();
+        cortex_m::asm::delay(200_000);
+        led.set_low().unwrap();
     }
 }
